fff868308e4819897befc3bf4eaae43736901765
Nguyen Cong Hieu <hieucong2468@gmail.com>
2025-06-30 15:15:40 +0700

    test

--- leave-management/createDatabase/SampleData.sql
+++
@@ -1,3 +0,0 @@
--- Insert sample leave request
-INSERT INTO Leave_Requests (user_id, start_date, end_date, reason, status, title)
-VALUES (5, '2025-06-21', '2025-06-23', 'Vacation', 'Inprogress', N'Xin nghỉ phép');
